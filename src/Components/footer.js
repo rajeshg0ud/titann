299d2ba209d9cb94dad2c2bca5757e88b6a9beec
unknown <budidirajeshgoud231@gmail.com>
2024-03-28 14:38:28 +0530

    logo

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -6,35 +6,35 @@ const Footer = () => {
   
     console.log("console from footer ");
   return (
-    <div className="h-64 bg-gray-100 font-semibold m-auto font-sans text-gray-500 pt-4">
+    <div className="h-64 bg-stone-200 font-semibold m-auto font-sans text-gray-600 pt-4">
       <div className="flex justify-around">
         <div className="px-2 sm:px-5">
-          <h3 className='text-xs sm:text-lg m-1 text-black'>Company</h3>
+          <h3 className='text-xs cursor-pointer sm:text-lg m-1 text-black'>Company</h3>
           <ul>
-            <li className=' text-xs sm:text-md m-3'>About Us</li>
-            <li className=' text-xs sm:text-md m-3'>Team</li>
-            <li className=' text-xs sm:text-md m-3'>Careers</li>
-            <li className=' text-xs sm:text-md m-3'>Blog</li>
+            <li className=' text-xs cursor-pointer sm:text-md m-3'>About Us</li>
+            <li className=' text-xs cursor-pointer sm:text-md m-3'>Team</li>
+            <li className=' text-xs cursor-pointer sm:text-md m-3'>Careers</li>
+            <li className=' text-xs cursor-pointer sm:text-md m-3'>Blog</li>
           </ul>
         </div>
         <div className="footer-column">
-          <h3 className='text-xs sm:text-lg m-1 text-black'>Contact</h3>
+          <h3 className='text-xs cursor-pointer sm:text-lg m-1 text-black'>Contact</h3>
           <ul>
-            <li className=' text-xs sm:text-md m-3'>Contact Us</li>
-            <li className=' text-xs sm:text-md m-3'>Help & Support</li>
-            <li className=' text-xs sm:text-md m-3'>FAQs</li>
+            <li className=' text-xs cursor-pointer sm:text-md m-3'>Contact Us</li>
+            <li className=' text-xs cursor-pointer sm:text-md m-3'>Help & Support</li>
+            <li className=' text-xs cursor-pointer sm:text-md m-3'>FAQs</li>
           </ul>
         </div>
         <div className="footer-column">
-          <h3 className='text-xs sm:text-lg m-1 text-black'>Connect with us</h3>
+          <h3 className='text-xs cursor-pointer sm:text-lg m-1 text-black'>Connect with us</h3>
           <div className="social-icons">
-            <FontAwesomeIcon className=' m-1 size-4 sm:size-6' icon={faFacebook} />
-            <FontAwesomeIcon className=' m-1 size-4 sm:size-6'  icon={faInstagram} />
-            <FontAwesomeIcon className=' m-1 size-4 sm:size-6' icon={faTwitter} />
+            <FontAwesomeIcon className=' m-1 size-4 sm:size-6 cursor-pointer' icon={faFacebook} />
+            <FontAwesomeIcon className=' m-1 size-4 sm:size-6 cursor-pointer'  icon={faInstagram} />
+            <FontAwesomeIcon className=' m-1 size-4 sm:size-6 cursor-pointer' icon={faTwitter} />
           </div>
         </div>
       </div>
-      <div className=" text-center m-5 text-xs sm:text-md">
+      <div className=" text-center m-5 text-xs cursor-pointer sm:text-md">
         <p>&copy; Titan App. All rights reserved.</p>
       </div>
     </div>
